611500aa758689cbb865b62861d6184db235c464
v <v@vbox.myguest.virtualbox.org>
2025-10-25 21:10:47 +0530

    simple commands added

--- client.cpp
+++ client.cpp
@@ -1,13 +1,93 @@
+/*
+key is an integer, and value is a string.
+
+create <key> <value>
+create 1 hello world
+
+read <key>
+read 1
+
+delete <key>
+delete 1
+*/
+
 #include "httplib.h"
 
+#define SERVER_ADDRESS "http://127.0.0.1:5000"
+
+
 int main() {
-    httplib::Client cli("http://localhost:5000"); // IP:Port of server.
-    auto res = cli.Get("/");
-    if (res) {
-        std::cout << res->status << "\n";
-        std::cout << res->body << "\n";
-    } 
-    else {
-        std::cout << "Request failed\n";
+    httplib::Client cli(SERVER_ADDRESS); // IP:Port of server.
+    std::string request;
+    std::string token;
+    httplib::Params params;
+    httplib::Response res;
+
+    while (1)
+    {
+        // read the input from user
+        std::cout << "$ ";
+        std::getline(std::cin, request);
+        
+        // get the command type
+        std::istringstream iss(request);
+        std::getline(iss, token, ' '); // breaks the string by ' ' and returns the answer in token
+
+        params.erase("key");
+        params.erase("value");
+
+        if (token == "create")
+        {
+            std::string key, value;
+            std::getline(iss, key, ' ');
+            std::getline(iss, value, '\0');
+            
+            params.emplace("key", key);
+            params.emplace("value", value);
+
+            auto res = cli.Post("/create", params);
+            if (res)
+            {
+                std::cout << res->status << "\n";
+                std::cout << res->body << "\n";
+            } 
+            else
+            {
+                std::cout << "Request failed\n";
+            }
+        }
+        else if (token == "read")
+        {
+            std::getline(iss, token, '\0'); //get the key
+
+            auto res = cli.Get("/read?key=" + token); // send the key via a GET request
+            if (res)
+            {
+                std::cout << res->status << "\n";
+                std::cout << res->body << "\n";
+            } 
+            else
+            {
+                std::cout << "Request failed\n";
+            }
+        }
+        else if (token == "delete")
+        {
+            std::getline(iss, token, '\0');
+            params.emplace("key", token);
+
+            auto res = cli.Post("/delete", params);
+            if (res)
+            {
+                std::cout << res->status << "\n";
+                std::cout << res->body << "\n";
+            } 
+            else
+            {
+                std::cout << "Request failed\n";
+            }
+        }
+        
     }
+
 }
--- server.cpp
+++ server.cpp
@@ -1,13 +1,41 @@
 #include "httplib.h"
 #include <iostream>
 
+#define IP "127.0.0.1"
+#define port 5000
+//check browser on http://127.0.0.1/5000
+
 int main() {
     httplib::Server svr;
 
-    svr.Get("/", [](const httplib::Request&, httplib::Response& res) {
-        res.set_content("Hello, cpp-httplib!", "text/plain");
+    svr.Get("/welcome", [](const httplib::Request&, httplib::Response& res) {
+        res.set_content("Hello, This is an http-based Key-Value server.", "text/plain");
+    });
+    
+    // For the "create" command
+    svr.Post("/create", [](const httplib::Request& req, httplib::Response& res) {
+        std::cout << "Received " << req.get_param_value("key") << " " << req.get_param_value("value") << "\n";
+        fflush(stdout);
+        res.set_content("Done", "text/plain");
     });
 
-    std::cout << "Server started at http://localhost:5000\n";
-    svr.listen("0.0.0.0", 5000); // IP:Port of server 
+    // For the "read" command
+    svr.Get("/read", [](const httplib::Request& req, httplib::Response& res) {
+        std::cout << "Received " << req.get_param_value("key") << "\n";
+        fflush(stdout);
+        res.set_content(req.body, "text/plain");
+    });
+
+    // For the "delete" command
+    svr.Post("/delete", [](const httplib::Request& req, httplib::Response& res) {
+        std::cout << "Received " << req.get_param_value("key") << " " << req.get_param_value("value") << "\n";
+        fflush(stdout);
+        res.set_content(req.body, "text/plain");
+    });
+
+    std::cout << "Server started at "<< IP << ":"<< port <<"\n";
+    fflush(stdout);
+
+    // Start listening to the server
+    svr.listen(IP, port); // IP:Port of server 
 }
